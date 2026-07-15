0b7860ea23f7455ea884ceb34ecffdab4f848deb
Kota Mizuno <mizuno9066@gmail.com>
2026-07-16 07:07:40 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// オフラインでも使えるようにするService Worker
+const CACHE = "tango1kyu-v1";
+const FILES = ["./", "index.html", "words.js", "manifest.json", "icon-192.png", "icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match(e.request, { ignoreSearch: true }))
+  );
+});
